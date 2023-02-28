ce860a0657bdfb0312ed0d257a97bd8e94c600f6
DEINERRV <deiner.ra10@gmail.com>
2023-02-28 16:17:54 -0600

    create expense router

---
+++ routes/expense.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+const {getAllExpenses,getExpense,createExpense,updateExpense,deleteExpense} = require("../controllers/expense");
+
+router.route("/")
+    .get(getAllExpenses)
+    .post(createExpense)
+;
+
+router.route("/:id")
+    .get(getExpense)
+    .patch(updateExpense)
+    .delete(deleteExpense)
+;
+
+module.exports = router;
